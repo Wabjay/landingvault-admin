325cbb92480730eb3e6629b84ff6bd71e135a832
wabjay <jefferyjohn230@gmail.com>
2024-11-09 20:26:07 +0100

    Updated some of the functionalities

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,19 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import ControlIcon from "@/assets/control.png";
 import ChartIcon from "@/assets/Chart.png";
 import SettingIcon from "@/assets/Setting.png";
-import LogoIcon from "@/assets/logo.png";
+import LogoIcon from "@/assets/logo.png"; // Uncomment if you plan to use logo
 import { handleLogout } from "@/lib/logout";
 import { useNavigation } from "./utils/navigations";
+import { usePathname } from "next/navigation";
 
 const SideBar = () => {
   const [isOpen, setIsOpen] = useState(true);
   const { navigateTo } = useNavigation();
   const { logOut } = handleLogout();
+  const pathname = usePathname();
 
   const menuItems = [
     { label: "Dashboard", href: "/", icon: ChartIcon },
@@ -27,17 +29,20 @@ const SideBar = () => {
   const toggleSidebar = () => setIsOpen(!isOpen);
 
   const handleLogoutClick = () => {
-    console.log("Logout button");
     logOut(navigateTo);
   };
+
+  // Check if the current pathname matches the item's href
+  const isActive = (href) => pathname.startsWith(href);
 
   return (
     <div className="flex">
       <div
         className={`${
           isOpen ? "w-[200px]" : "w-20"
-        } bg-white border-r border-r-green-600 h-full p-5 pt-4 relative duration-300`}
+        } bg-white border-r border-r-green-600 h-full pt-4 relative duration-300`}
       >
+        {/* Sidebar toggle button */}
         <Image
           src={ControlIcon}
           alt="Toggle Sidebar"
@@ -47,6 +52,7 @@ const SideBar = () => {
           onClick={toggleSidebar}
         />
 
+        {/* Logo and brand name */}
         <div className="flex gap-x-2 items-center">
           {/* <Image
             src={LogoIcon}
@@ -56,25 +62,32 @@ const SideBar = () => {
             }`}
           /> */}
           {isOpen && (
-            <h1 className="text-green-600 font-semibold text-16 whitespace-nowrap">
+            <h1 className="text-green-600 p-5 font-semibold text-16 whitespace-nowrap">
               LANDINGVAULT
             </h1>
           )}
         </div>
 
+        {/* Menu items */}
         <ul className="pt-6 relative h-full">
           {menuItems.map((item) => (
             <Link href={item.href} key={item.label}>
               <li
-                className="flex rounded-md p-2 cursor-pointer hover:bg-white hover:underline text-green-600 text-xl font-semibold items-center gap-x-4 mb-2 bg-light-white"
+                className={`${
+                  isActive(item.href)
+                    ? "bg-[#000] text-white"
+                    : "bg-white text-green-600"
+                } border border-black flex p-2 cursor-pointer hover:bg-[#000] hover:text-white text-xl font-semibold items-center gap-x-4`}
               >
-                <Image src={item.icon} alt={`${item.label} Icon`} />
+                {/* <Image src={item.icon} alt={`${item.label} Icon`} /> */}
                 {isOpen && (
                   <span className="origin-left duration-200">{item.label}</span>
                 )}
               </li>
             </Link>
           ))}
+
+          {/* Logout button */}
           <li
             onClick={handleLogoutClick}
             className="mt-8 flex rounded-md p-2 cursor-pointer hover:bg-white hover:underline text-green-600 text-xl font-semibold items-center gap-x-4"
